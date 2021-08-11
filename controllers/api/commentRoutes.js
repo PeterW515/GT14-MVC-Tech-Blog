75fdb391d1d1527c357ef83d9d8ce532d2984f66
Peter Weigman <phweigman@gmail.com>
2021-08-11 16:02:04 -0400

    working comment functionality

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,7 +3,10 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
+
+  
   try {
+
     const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,22 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#post-title').value.trim();
-  const content = document.querySelector('#post-content').value.trim();
+  const post_id = window.location.pathname.substr(window.location.pathname.indexOf('path/'));
+  const content = document.querySelector('#comment-content').value.trim();
 
-  if (title && content) {
-    const response = await fetch(`/api/posts`, {
+  if (content) {
+    const response = await fetch(`/api/comments`, {
       method: 'POST',
-      body: JSON.stringify({ title, content }),
+      body: JSON.stringify({ content, post_id }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace(`${window.location.pathname}`);
     } else {
-      alert('Failed to create post');
+      alert('Failed to create comment');
     }
   }
 };
@@ -38,9 +38,6 @@ const delButtonHandler = async (event) => {
 };
 
 document
-  .querySelector('.new-post-form')
+  .querySelector('.new-comment-form')
   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.post-list')
-  .addEventListener('click', delButtonHandler);
